b7c24d832ebd6f0edb64f03379e78b04eee260c8
igorperez <iperez22@izarraitz.eus>
2023-01-26 12:19:51 +0100

    igor

--- COPIAK/PegatinaCOPIA.java
+++ COPIAK/PegatinaCOPIA.java
@@ -2,8 +2,8 @@ public class Pegatina {
     
     private Integer id_ticketa;
     private String izena;
-    private LocalDate date;
-    private LocalTime ordua;
+  //  private LocalDate date;
+ //   private LocalTime ordua;
     private double pisua;
     private double prezioa_kiloko;
     private double prezio_totala;
@@ -16,8 +16,8 @@ public class Pegatina {
     public Pegatina(Integer id_ticketa,String izena, LocalDate date, LocalTime ordua,double pisua, double prezioa_kiloko,double prezio_totala,Integer id_denda,Integer id_langilea){
         this.id_ticketa =id_ticketa;
         this.izena= izena;
-        this.date = date;
-        this.ordua =ordua;
+     //   this.date = date;
+     //   this.ordua =ordua;
         this.pisua =pisua;
         this.prezioa_kiloko =prezioa_kiloko;
         this.prezio_totala=prezio_totala;
