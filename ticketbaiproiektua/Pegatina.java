501cdafc689d7278b355998ea01974d989ed6097
eberkin <equintana22@izarraitz.eus>
2023-01-30 13:45:47 +0100

    eber

--- ticketbaiproiektua/Pegatina.java
+++ ticketbaiproiektua/Pegatina.java
@@ -30,7 +30,51 @@ public class Pegatina {
         this.id_denda = id_denda;
         this.id_langilea=id_langilea;
     }
-    public void setEtiketa(String lerroa) {
+    public void setEtiketa(String lerroa, String pathFitxategia) {
+        this.date=pathFitxategia;
+        String info = lerroa;
+
+        String izena = info.substring(0, info.indexOf("$"));
+        info = info.substring(info.indexOf("$"));
+    
+        String saltzaileID = info.substring(0, info.indexOf("$"));
+        info = info.substring(info.indexOf("$"));
+    
+        String prezioaKiloko =info.substring(0, info.indexOf("$"));
+        info = info.substring(info.indexOf("$"));
+    
+        String pisua= info.substring(0, info.indexOf("$"));
+        info = info.substring(info.indexOf("$"));
+    
+        String prezioTotala=info.substring(0, info.indexOf("$"));
+        info = info.substring(info.indexOf("$"));
+        //variable sortu konstruktorearen argumentuak variasble batean gordetzeko
+    
+        int id_ticketa=Integer.parseInt(saltzaileID);
+        double pisuaDouble= Double.parseDouble(pisua);
+        double prezioaKilokoDouble=Double.parseDouble(prezioaKiloko);
+        double prezioTotalaDouble=Double.parseDouble(prezioTotala);
+        //id denda eta id_langilea variableak sartu eta definituak ez badaude definitu
+        int id_denda = 0;
+        int id_langilea = 0;
+        //pegatina objetua sortu
+       // Pegatina pgtn = new Pegatina(id_ticketa, izena, null, null, pisuaDouble, prezioaKilokoDouble, prezioTotalaDouble, id_denda, id_langilea);
+    
+        //return pgtn;
+        return null;
+
     }
+
+    private String DataAtera( String Dokumentuizena) {
+  
+  
+        String urtea = Dokumentuizena.substring(0, 4);
+        String hilabetea = Dokumentuizena.substring(5, 6);
+        String eguna = Dokumentuizena.substring(7, 8);
+        String ordua = Dokumentuizena.substring(9, 10);
+        String minutua = Dokumentuizena.substring(11, 12);
+        String segundua = Dokumentuizena.substring(13, 14);
+         return Dokumentuizena;
+       }
 
 }
