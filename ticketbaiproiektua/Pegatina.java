6c5187a58f0e6d1b032258e26e11b3b9854be6fa
eberkin <equintana22@izarraitz.eus>
2023-01-31 10:32:49 +0100

    eberchange

--- ticketbaiproiektua/Pegatina.java
+++ ticketbaiproiektua/Pegatina.java
@@ -1,14 +1,11 @@
 package ticketbaiproiektua;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+
 
 public class Pegatina {
     
     private Integer id_ticketa;
     private String izena;
-    private LocalDate date;
-    private LocalTime ordua;
     private double pisua;
     private double prezioa_kiloko;
     private double prezio_totala;
@@ -22,8 +19,6 @@ public class Pegatina {
     public Pegatina(Integer id_ticketa,String izena,double pisua, double prezioa_kiloko,double prezio_totala,Integer id_denda,Integer id_langilea){
         this.id_ticketa =id_ticketa;
         this.izena= izena;
-        this.date = date;
-        this.ordua = ordua;
         this.pisua =pisua;
         this.prezioa_kiloko =prezioa_kiloko;
         this.prezio_totala=prezio_totala;
@@ -31,7 +26,6 @@ public class Pegatina {
         this.id_langilea=id_langilea;
     }
     public void setEtiketa(String lerroa, String pathFitxategia) {
-        this.date=pathFitxategia;
         String info = lerroa;
 
         String izena = info.substring(0, info.indexOf("$"));
@@ -59,15 +53,16 @@ public class Pegatina {
         int id_langilea = 0;
         //pegatina objetua sortu
        // Pegatina pgtn = new Pegatina(id_ticketa, izena, null, null, pisuaDouble, prezioaKilokoDouble, prezioTotalaDouble, id_denda, id_langilea);
-    DataAtera(pathFitxategia);
-        //return pgtn;
-        return null;
+        //DatuAtera funtziora deituko diogu eta pathfitxategia pasatuko dio denbora ateratzeko
+       DataAtera(pathFitxategia);
+       
+        
 
     }
 
     private String DataAtera( String pathfitxategiaString) {
   
-  
+        //String ezberdinak sortzen dira eta substringekin ezartzen diogu zer den bakoitza
         String urtea = pathfitxategiaString.substring(0, 4);
         String hilabetea = pathfitxategiaString.substring(5, 6);
         String eguna = pathfitxategiaString.substring(7, 8);
