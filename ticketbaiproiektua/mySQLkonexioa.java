7c1728b6cec4f8421482e0e9018aa789d943026f
MaRii0NiiSTaL <mnistal22@izarraitz.eus>
2023-02-13 10:27:44 +0100

    jansaj jd

--- ticketbaiproiektua/mySQLkonexioa.java
+++ ticketbaiproiektua/mySQLkonexioa.java
@@ -1,50 +1,19 @@
 package ticketbaiproiektua;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-
-
-public class mySQLkonexioa {    
-
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private Connection conn;
-
-   public mySQLkonexioa() {
-      conn = null;
-   }
-
-   public Connection getConnection() {
-      if (conn == null) {
-         try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection("jdbc:mysql://192.168.:3306/salmentak", "root", "Pa$$w0rd");
-            System.out.println("ONDO KONEKTATU DA");
-         } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-             System.out.println("EZIN IZAN DA KONEKTATU");
+public void adduser(User s) {
+    try {
+         sql = "insert into users ('u_fname', 'u_lname', 'u_uname', 'u_pass', 'u_age', 'u_adderess')"
+                 + "values('" + s.getFirstname() + "','" + s.getLastname()
+                 + "','" + s.getUsername() + "','" + s.getPassword() + "','" + s.getAge() + "','" + s.getAdderss() + "')";
+         stmt = conn.createStatement();
+         int i = stmt.executeUpdate(sql);
+         if (i > 0) {
+             System.out.println("ROW INSERTED");
+         } else {
+             System.out.println("ROW NOT INSERTED");
          }
-      }
-      return conn;
-   }
-
-   public void closeConnection() {
-      if (conn != null) {
-         try {
-            conn.close();
-            conn = null;
-            System.out.println("ONDO DESKONEKTATU DA");
-         } catch (SQLException e) {
-            e.printStackTrace();
-         }
-      }
-   }
-
-    Statement createStatement() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-
-}
+     } catch (Exception e) {
+         System.out.println(e);
+     }
+ 
+ }
