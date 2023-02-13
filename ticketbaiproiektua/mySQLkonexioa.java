7912a411e5a33031ceadfcc264a73b471745b9d4
eberkin <equintana22@izarraitz.eus>
2023-02-13 11:41:24 +0100

    mysql

--- ticketbaiproiektua/mySQLkonexioa.java
+++ ticketbaiproiektua/mySQLkonexioa.java
@@ -1,28 +1,26 @@
 package ticketbaiproiektua;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Connection;
+import java.sql.*;
 
-public class mySQLkonexioa {
-    
-    public static Connection getConector(String servidor,String bd,String user,String pswd) {
-        Connection conexion = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            String URL="jdbc:mysql://"+servidor+":8889/"+bd;
-            conexion=DriverManager.getConnection(URL,user,pswd);
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return conexion;
-    }
+public class Conexion {
+private static Connection cnx = null;
+public static Connection obtener() throws SQLException,  ClassNotFoundException {
+  if (cnx == null) {
+     try {
+        Class.forName("com.mysql.jdbc.Driver");
+        cnx = DriverManager.getConnection("jdbc:mysql://localhost/java_mysql", "root", "");
+     } catch (SQLException ex) {
+        throw new SQLException(ex);
+     } catch (ClassNotFoundException ex) {
+        throw new ClassCastException(ex.getMessage());
+     }
+  }
+  return cnx;
+  }
 
+public static void cerrar() throws SQLException {
+  if (cnx != null) {
+     cnx.close();
+  }
+  }
 }
