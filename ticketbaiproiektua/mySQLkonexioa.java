e4912953dda32273af0b6561a03cb265d8635b5c
eberkin <equintana22@izarraitz.eus>
2023-02-15 08:54:21 +0100

    MYSQL

--- ticketbaiproiektua/mySQLkonexioa.java
+++ ticketbaiproiektua/mySQLkonexioa.java
@@ -16,16 +16,11 @@ public class mySQLkonexioa {
    try {
        Connection conn = DriverManager.getConnection(url, user, password);
        sentencia = conn.createStatement();
-       sql = "NSERT INTO TICKETBAI.TICKETA (idTICKETA, izena, urtea_data, hilabetea_data, eguna_data, pisua, prezioa_kiloko, prezio_totala, idLANGILEA, idPRODUKTUAK, idDENDA) VALUES (1, 'Ticket1', 2023, 2, 15, 0.5, 3.5, 1.75, 1, 1, 1);";
-       rs = sentencia.executeQuery(sql);
-       while(rs.next()){
-         int idTICKETA = rs.getInt("idTICKETA");
-         String izena = rs.getString("izena");
-         System.out.println(" ");
-       }
+       sql = "INSERT INTO TICKETBAI.TICKETA (idTICKETA, izena, urtea_data, hilabetea_data, eguna_data, pisua, prezioa_kiloko, prezio_totala, idLANGILEA, idPRODUKTUAK, idDENDA) VALUES (1, 'Marioperri', 2023, 2, 15, 0.5, 3.5, 1.75, 1, 1, 1);";
+       sentencia.executeUpdate(sql);
        conn.close();
-      
-   } catch (SQLException e) {
+       }
+        catch (SQLException e) {
        System.out.println("Error al conectarse a la base de datos: " + e.getMessage());
    }
 }
