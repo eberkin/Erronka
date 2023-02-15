d293a0e15859be2ce2ba12cf91f967f2c22ee956
eberkin <equintana22@izarraitz.eus>
2023-02-15 08:57:29 +0100

    v

--- ticketbaiproiektua/mySQLkonexioa.java
+++ ticketbaiproiektua/mySQLkonexioa.java
@@ -16,7 +16,7 @@ public class mySQLkonexioa {
    try {
        Connection conn = DriverManager.getConnection(url, user, password);
        sentencia = conn.createStatement();
-       sql = "INSERT INTO TICKETBAI.TICKETA (idTICKETA, izena, urtea_data, hilabetea_data, eguna_data, pisua, prezioa_kiloko, prezio_totala, idLANGILEA, idPRODUKTUAK, idDENDA) VALUES (1, 'Marioperri', 2023, 2, 15, 0.5, 3.5, 1.75, 1, 1, 1);";
+       sql = "INSERT INTO TICKETBAI.TICKETA (idTICKETA, izena, urtea_data, hilabetea_data, eguna_data, pisua, prezioa_kiloko, prezio_totala, idLANGILEA, idPRODUKTUAK, idDENDA) VALUES (2, 'Marioperri', 2023, 2, 15, 0.5, 3.5, 1.75, 1, 1, 1);";
        sentencia.executeUpdate(sql);
        conn.close();
        }
